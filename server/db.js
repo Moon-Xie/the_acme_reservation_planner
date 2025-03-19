857d4fd432520925b3442ed430495fbc0931144f
Yue Xie <moonxie9304@gmail.com>
2025-03-19 18:48:07 +0700

    added database functions

--- server/db.js
+++ server/db.js
@@ -1,7 +1,106 @@
 const pg = require('pg')
 const DATABASE_URL = process.env.DATABASE_URL
+const uuid = require('uuid')
 const client = new pg.Client(DATABASE_URL)
 
+const createTables = async () => {
+    const SQL =/*sql*/ `
+        DROP TABLE IF EXISTS reservations;
+        DROP TABLE IF EXISTS customer;
+        DROP TABLE IF EXISTS restaurant;
+        CREATE TABLE customer(
+            id UUID PRIMARY KEY,
+            name VARCHAR(100) NOT NULL
+        );
+        CREATE TABLE restaurant(
+            id UUID PRIMARY KEY,
+            name VARCHAR(255) NOT NULL
+        );
+        CREATE TABLE reservations(
+            id UUID PRIMARY KEY,
+            date DATE NOT NULL,
+            party_count INTEGER NOT NULL,
+            restaurant_id UUID REFERENCES restaurant(id) NOT NULL,
+            customer_id UUID REFERENCES customer(id) NOT NULL
+        )
+    `
+    await client.query(SQL)
+}
+
+const createCustomer = async (name) => {
+    const SQL = /*sql*/`
+        INSERT INTO customer(id, name) 
+        VALUES ($1, $2)
+        RETURNING *;
+    `
+    const values = [uuid.v4(), name]
+    const response = await client.query(SQL, values)
+    return response.rows[0]
+}
+
+const createRestaurant = async (name) => {
+    const SQL = /*sql*/`
+        INSERT INTO restaurant (id, name) 
+        VALUES ($1, $2) 
+        RETURNING *;
+    `
+    const values = [uuid.v4(), name]
+    const response = await client.query(SQL, values)
+    return response.rows[0]
+}
+
+const createReservation = async ({date, party_count, restaurant_id, customer_id}) => {
+    const SQL = /*sql*/ `
+        INSERT INTO reservations (id, date, party_count, restaurant_id, customer_id)
+        VALUES ($1, $2, $3, $4, $5) 
+        RETURNING *
+    `
+    const values = [uuid.v4(), date,party_count, restaurant_id, customer_id]
+    const response = await client.query(SQL, values)
+    return response.rows[0]
+}
+
+const fetchCustomers = async () => {
+    const SQL = /*sql*/ `
+        SELECT * FROM customer;
+    `
+    const response = await client.query(SQL)
+    return response.rows
+}
+
+const fetchRestaurants = async () => {
+    const SQL = /*sql*/ `
+        SELECT * FROM restaurant;
+    `
+    const response = await client.query(SQL)
+    return response.rows
+}
+
+const fetchReservations = async () => {
+    const SQL = /*sql*/ `
+        SELECT * FROM reservation;
+    `
+    const response = await client.query(SQL)
+    return response.rows
+}
+
+const destroyReservation = async ({customer_id, id}) => {
+    const SQL = /*sql*/ `
+        DELETE FROM reservation 
+        WHERE customer_id = $1 AND id = $2
+    `
+    const values = [customer_id, id]
+    await client.query(SQL, values)
+}
+
 module.exports = {
-    client
+    client,
+    createTables,
+    createCustomer,
+    createRestaurant,
+    createReservation,
+    fetchCustomers,
+    fetchRestaurants,
+    fetchReservations,
+    destroyReservation
 }
--- server/index.js
+++ server/index.js
@@ -6,13 +6,43 @@ app.use(morgan('dev'))
 app.use(express.json())
 const PORT = process.env.PORT
 const {
-    client
+    client,
+    createTables,
+    createCustomer,
+    createRestaurant,
+    createReservation
 } = require('./db')
 
 const init = async () => {
     console.log('Connecting with database...')
     await client.connect()
     console.log('Database connected!')
+    await createTables()
+    console.log('Created tables!')
+    const [Moon, Hunter, Bojack, whyNot, appleBee, buffet] = await Promise.all([
+        createCustomer("Moon"),
+        createCustomer("Hunter"),
+        createCustomer("Bojack"),
+        createRestaurant("Why Not"),
+        createRestaurant("AppleBee"),
+        createRestaurant("Buffet")
+    ])
+    
+    const [reservation, reservation1] = await Promise.all([
+        createReservation({
+            date: '4/18/2025',
+            party_count: 3,
+            restaurant_id: appleBee.id,
+            customer_id: Moon.id
+        }),
+        createReservation({
+            date: '5/30/2025',
+            party_count: 3,
+            restaurant_id: buffet.id,
+            customer_id: Bojack.id
+        })
+    ])
+    
     app.listen(PORT, () => {
         console.log('Server is listening to', PORT)
     })
